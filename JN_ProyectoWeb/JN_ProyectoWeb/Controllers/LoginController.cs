a23b3e90fb5339bb428ecfe15e28a7a5581a70f1
JPVindas <jvindas40886@ufide.ac.cr>
2025-02-06 17:35:36 -0600

    semana 4

---
+++ JN_ProyectoWeb/JN_ProyectoWeb/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarContrasenna()
+        {
+            return View();
+        }
+    }
+}
